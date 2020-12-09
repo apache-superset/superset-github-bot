10c21af8d301769c40c8ef4d5c3c704671e6abec
Evan Rusackas <evan@preset.io>
2020-12-09 13:17:17 -0800

    switching to semistandard, to get our semicolons back!

--- src/index.js
+++ src/index.js
@@ -1,60 +1,60 @@
-const conf = require('./config.js')
+const conf = require('./config.js');
 
 async function assignOrgLabel (context, conf) {
-  const G = context.github
-  const issueOrPR = context.payload.issue || context.payload.pull_request
-  const username = issueOrPR.user.login
-  const orgs = await G.orgs.listForUser({ username })
-  const orgNames = orgs.data.map(v => v.login)
-  const issueCtx = context.issue()
+  const G = context.github;
+  const issueOrPR = context.payload.issue || context.payload.pull_request;
+  const username = issueOrPR.user.login;
+  const orgs = await G.orgs.listForUser({ username });
+  const orgNames = orgs.data.map(v => v.login);
+  const issueCtx = context.issue();
   // get list of matching github orgs
-  let matchingOrgs = orgNames.filter(org => conf.ORG_WHITELIST.includes(org))
+  let matchingOrgs = orgNames.filter(org => conf.ORG_WHITELIST.includes(org));
   // append user-based orgs from list
   if (conf.USER_ORG_XREF[username]) {
-    matchingOrgs.push(conf.USER_ORG_XREF[username])
+    matchingOrgs.push(conf.USER_ORG_XREF[username]);
   }
   // look up org groups, add 'em in!
-  const orgGroups = []
+  const orgGroups = [];
   matchingOrgs.forEach(org => {
     if (conf.ORG_GROUPS[org]) {
-      orgGroups.push(conf.ORG_GROUPS[org])
+      orgGroups.push(conf.ORG_GROUPS[org]);
     }
-  })
-  matchingOrgs.concat(orgGroups)
+  });
+  matchingOrgs.concat(orgGroups);
   // dedupe, just in case
-  matchingOrgs = new Set(matchingOrgs)
+  matchingOrgs = new Set(matchingOrgs);
   matchingOrgs.forEach(org => {
     return G.issues.addLabels(
-      context.issue({ issue_number: issueCtx.number, labels: [org] }))
-  })
+      context.issue({ issue_number: issueCtx.number, labels: [org] }));
+  });
 }
 async function emojiLabel (context) {
-  const issueCtx = context.issue()
-  const comment = context.payload.comment.body.toLowerCase()
-  console.log(`Looking for labels in comment: "${comment}"`)
+  const issueCtx = context.issue();
+  const comment = context.payload.comment.body.toLowerCase();
+  console.log(`Looking for labels in comment: "${comment}"`);
   if (context.payload.comment.user.type !== 'Bot') {
     // const match = comment.match(/\u{1F3F7}\s*(\S+)\s*(\n|$)/u);
-    const match = comment.match(/🏷\s*(.+)\s*(\n|$)/)
+    const match = comment.match(/🏷\s*(.+)\s*(\n|$)/);
     if (match) {
-      const label = match[1].replace(String.fromCharCode(65039), '').trim()
-      console.log(`Found label: "${label}"`)
-      const isValidTag = label.match(/^([a-z0-9-.:]+)$/i)
+      const label = match[1].replace(String.fromCharCode(65039), '').trim();
+      console.log(`Found label: "${label}"`);
+      const isValidTag = label.match(/^([a-z0-9-.:]+)$/i);
       if (isValidTag) {
-        console.log('Label is valid!')
+        console.log('Label is valid!');
         if (comment.includes('🗑')) {
-          console.log('Removing label')
+          console.log('Removing label');
           return context.github.issues.removeLabel(
-            context.issue({ issue_number: issueCtx.number, name: label }))
+            context.issue({ issue_number: issueCtx.number, name: label }));
         } else {
-          console.log('Adding label')
+          console.log('Adding label');
           return context.github.issues.addLabels(
-            context.issue({ issue_number: issueCtx.number, labels: [label] }))
+            context.issue({ issue_number: issueCtx.number, labels: [label] }));
         }
       } else {
-        console.log('Label is not valid')
+        console.log('Label is not valid');
       }
     } else {
-      console.log("Didn't find a proper label emoji")
+      console.log("Didn't find a proper label emoji");
     }
   }
 }
@@ -66,29 +66,29 @@ async function emojiLabel (context) {
 
 module.exports = app => {
   app.on('issues.opened', async context => {
-    assignOrgLabel(context, conf)
-  })
+    assignOrgLabel(context, conf);
+  });
 
   app.on('pull_request.opened', async context => {
-    assignOrgLabel(context, conf)
-  })
+    assignOrgLabel(context, conf);
+  });
 
   app.on('issue_comment.created', async context => {
-    emojiLabel(context)
-  })
+    emojiLabel(context);
+  });
 
   app.on('pull_request_review_comment.created', async context => {
-  })
-}
+  });
+};
 
 if (conf.HEROKU_APP_NAME) {
-  const http = require('http')
+  const http = require('http');
   setInterval(() => {
-    console.log('WAKE UP HEROKU!')
+    console.log('WAKE UP HEROKU!');
     try {
-      http.get(`http://${conf.HEROKU_APP_NAME}.herokuapp.com`)
+      http.get(`http://${conf.HEROKU_APP_NAME}.herokuapp.com`);
     } catch (e) {
-      console.error('HTTP get failed...')
+      console.error('HTTP get failed...');
     }
-  }, conf.HEROKU_WAKEUP_FREQUENCY)
+  }, conf.HEROKU_WAKEUP_FREQUENCY);
 }
