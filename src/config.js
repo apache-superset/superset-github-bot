85572636b3eda9d34f207ace2e0324b3e454fb73
Evan Rusackas <evan@preset.io>
2020-12-09 23:56:25 -0800

    a more specific org-group label

--- src/config.js
+++ src/config.js
@@ -6,9 +6,9 @@ exports.USER_ORG_XREF = {
   nikolagigic: 'Flexiana'
 };
 exports.ORG_GROUPS = {
-  Flexiana: 'ext',
-  Turing: 'ext',
-  Polidea: 'ext'
+  Flexiana: 'preset-ext',
+  Turing: 'preset-ext',
+  Polidea: 'preset-ext'
 };
 // Heroku app needs to be woken up periodically to prevent it to go to sleep
 // https://quickleft.com/blog/6-easy-ways-to-prevent-your-heroku-node-app-from-sleeping/
