549bdc2c8a29e99a8ec42a74dbc1aa6ebc80e360
Arash <arash.afghahi@gmail.com>
2021-12-08 16:56:07 -0500

    adding superset partners as a whitelisted org

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-exports.ORG_WHITELIST = ['preset-io'];
+exports.ORG_WHITELIST = ["preset-io", "Superset-Community-Partners"];
 
 // Heroku app needs to be woken up periodically to prevent it to go to sleep
 // https://quickleft.com/blog/6-easy-ways-to-prevent-your-heroku-node-app-from-sleeping/
-exports.HEROKU_APP_NAME = 'damp-spire-74868'; // Set to falsy to stop pings
+exports.HEROKU_APP_NAME = "damp-spire-74868"; // Set to falsy to stop pings
 exports.HEROKU_WAKEUP_FREQUENCY = 300000; // every 5 minutes
